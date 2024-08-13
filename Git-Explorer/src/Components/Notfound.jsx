eaec4635ca5d915e1886eb82155a99b03d574f16
Govind Mohan Shah <www.govind020@gmail.com>
2024-08-13 22:58:15 +0545

    added

--- Git-Explorer/src/Components/Notfound.jsx
+++ Git-Explorer/src/Components/Notfound.jsx
@@ -1,9 +1,10 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import { Link } from 'react-router-dom'
 function Notfound() {
   return (
     <div>
-        <h1>Notfound</h1>
+        <h1>Page Not found</h1>
     <Link to="/">Go to Home page</Link>
     </div>
   );
